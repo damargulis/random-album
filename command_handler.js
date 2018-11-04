86979fad9c0a1bcb58d5ac5a9b0b18cd8cafdfbb
Daniel Margulis <damargulis@wustl.edu>
2018-11-04 12:01:04 -0500

    init

---
+++ command_handler.js
@@ -0,0 +1,19 @@
+chrome.commands.onCommand.addListener((command) => {
+    console.log('sending command');
+    console.log(command);
+    switch (command) {
+        case "next-album":
+            chrome.tabs.query({url: "*://*.google.com/music/listen*"}, (tabs) => {
+                chrome.tabs.sendMessage(tabs[0].id, {'type': 'next-album'});
+            });
+            break;
+        case "prev-album":
+            chrome.tabs.query({url: "*://*.google.com/music/listen*"}, (tabs) => {
+                chrome.tabs.sendMessage(tabs[0].id, {'type': 'prev-album'});
+            });
+            break;
+        default:
+            console.log("Command not recognized");
+            console.log(command);
+    }
+})
---
+++ random_album.js
@@ -0,0 +1,53 @@
+function playAlbum() {
+    const timerId = setInterval(() => {
+        const playButton = document.getElementById('playButton');
+        if (playButton) {
+            clearInterval(timerId);
+            playButton.click();
+        }
+    }, 100);
+}
+
+function nextAlbum() {
+    if (playlist[currentlyPlaying+1]) {
+        currentlyPlaying++;
+        window.location.href = playlist[currentlyPlaying];
+        playAlbum();
+    } else {
+        window.location.href = "https://play.google.com/music/listen#/albums";
+        const shuffleButton = document.querySelector('[data-id="shuffle-my-library"]')
+        shuffleButton.click();
+        const albumDetails = document.getElementsByClassName("player-album");
+        albumDetails[0].click();
+        playAlbum();
+        playlist.push(window.location.href);
+        currentlyPlaying++;
+    }
+}
+
+function prevAlbum() {
+    if (playlist[currentlyPlaying-1]) {
+        currentlyPlaying--;
+        window.location.href = playlist[currentlyPlaying];
+        playAlbum();
+    } else {
+        console.log('No previous album');
+    }
+}
+
+const playlist = [];
+let currentlyPlaying = -1;
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log('received command');
+    switch (request.type) {
+        case 'next-album':
+            console.log('calling next album');
+            nextAlbum();
+            break;
+        case 'prev-album':
+            prevAlbum();
+            break;
+        default:
+            console.log("Message not recoginzed");
+    }
+});
